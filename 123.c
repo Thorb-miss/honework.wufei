89f2d147e8600b6ef6af8e5f59a1639e29fb1777
Your Name <2961264556@qq.com>
2019-11-12 16:59:50 +0800

    happy1

--- 123.c
+++ 123.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-    int n,multiply;
+    int n,multiply=1;
     int i;
     printf("Enter n:\n");
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=1;i<=5;i++)
     {
         multiply=multiply*i;
     }
