133af7e12a2f21e3394998ff5bc708ebdc2d2303
Your Name <2961264556@qq.com  git config --global user.name Your>
2019-11-18 09:09:19 +0800

    test

---
+++ probably test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int i,n,f1=1,f2=1,f3;
+    double fact;
+    printf("Enter n:\n");
+    scanf("%d",&n);
+
+    printf("%d%d",f1,f2);
+    for(i=3;i<=n;i++)
+    {
+        f3=f1+f2;
+        fact=f2*1.0/f3;
+        f1=f2;
+        f2=f3;
+    }
+    printf("%d",f3);
+    return 0;
+}
