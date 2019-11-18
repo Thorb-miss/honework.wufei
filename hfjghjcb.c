8b78ae96fd69d583cf3af5f16e98d8d4f339edf5
Your Name <2961264556@qq.com  git config --global user.name Your>
2019-11-18 09:10:20 +0800

    xhggkvb

--- hfjghjcb.c
+++ hfjghjcb.c
@@ -3,6 +3,7 @@
 int main(void)
 {
     int i,n,f1=1,f2=1,f3;
+    double fact;
     printf("Enter n:\n");
     scanf("%d",&n);
 
@@ -10,6 +11,7 @@ int main(void)
     for(i=3;i<=n;i++)
     {
         f3=f1+f2;
+        fact=f2*1.0/f3;
         f1=f2;
         f2=f3;
     }
