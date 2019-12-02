7ddcff729f7cc0ec3f5e95bc4168f42222269030
Throb-miss <2961264556@qq.com>
2019-12-02 08:13:32 +0800

    baiqianbaiji2

--- baiqianbaiji.c
+++ baiqianbaiji.c
@@ -2,12 +2,12 @@
 int main(void)
 {
     int x,y, z;
-    for(x=1;x<=20;x++)
-    for(y=1;y<=33;y++)
+    for(x=0;x<=20;x++)
+    for(y=0;y<=33;y++)
     for(z=3;z<=99;z++)
     {
-        if((5*x+3*y+z/3==100)&&(x+y+z==100))
-        printf("%d,y=%d,z=%d\n",x,y,z);
+        if((5*x+3*y+z/3==100)&&(x+y+z==100)&&(z%3==0))
+        printf("x=%d,y=%d,z=%d\n",x,y,z);
 
     }
     return 0;
