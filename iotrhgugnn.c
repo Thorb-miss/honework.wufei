61d118f7ca40b50628b99b5e88157356644dfb7e
Your Name <2961264556@qq.com>
2019-12-10 17:29:37 +0800

    headach

--- iotrhgugnn.c
+++ iotrhgugnn.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
-#define LEN 10
+#define LEN 5
 
 int main(void)
 {
-   int a[LEN]={0},i;
+   int a[LEN]={0},i,max;
    printf("Enter array a:\n");
    for(i =0;i <LEN;i++)
    {
      scanf("%d",&a[i]);
 
    }
-   for(i=LEN-1;i>=0;i--)
-   {
-   printf("%5d",a[i]);
-   }
 
+   max =a[0];
+   for(i=1;i<LEN;i++)
+   {
+       if( a[i]>max){
+           max=a[i];
+
+       }
+
+   }
+  printf("Max is %d\n",max);
     return 0;
 }
