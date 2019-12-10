f4d7360f93e6f8f05c3a690d9ee165d3c63c9456
Your Name <2961264556@qq.com>
2019-12-10 17:19:53 +0800

    GWER

--- iotrhgugnn.c
+++ iotrhgugnn.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-#define N 10
+#define LEN 10
 
 int main(void)
 {
-   int a[N],i;
+   int a[LEN]={0},i;
    printf("Enter array a:\n");
-   for(i = 0;i < N;i++)
+   for(i =0;i <LEN;i++)
    {
      scanf("%d",&a[i]);
 
    }
-   for(i=0;i<N;i++)
+   for(i=LEN-1;i>=0;i--)
    {
    printf("%5d",a[i]);
    }
